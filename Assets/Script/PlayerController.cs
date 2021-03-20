78f3222e4077bed957b911b3293e2524dbad7973
FayOsan <fayosan15@gmail.com>
2021-03-19 22:49:40 -0700

    Initial commit

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rbPlayer;
+    public float gravityModifer;
+    public float jumpForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbPlayer = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool spaceDown = Input.GetKeyDown(KeyCode.Space);
+        if(spaceDown)
+        {
+            rbPlayer.AddForce(Vector3.up * jumpForce,  ForceMode.Impulse);
+        }
+    }
+}
